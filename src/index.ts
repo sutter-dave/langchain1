cfbd995f85bda07dde99b8dca688f68181283d1a
Dave Sutter <sutter@intransix.com>
2023-04-27 18:26:49 -0700

    more screwing around

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-import { PDFLoader } from "langchain/document_loaders";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as dotenv from "dotenv";
+import { OpenAI } from "langchain/llms/openai";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf"; // document loader
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"; // text splitter
+import { MemoryVectorStore } from "langchain/vectorstores/memory"; // vector db
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"; // embeddings
+import { RetrievalQAChain } from "langchain/chains";
 
-const loader = new PDFLoader("data/stormDataDef.pdf");
+// config secrets
+dotenv.config();
 
+// instantiate a model
+const model = new OpenAI({
+  modelName: "gpt-3.5-turbo",
+  openAIApiKey: process.env.OPENAI_API_KEY,
+});
+
+
+// load a document
+const loader = new PDFLoader("data/Micello WorkingSet Data Framework.pdf");
 const docs = await loader.load();
-console.log(docs !== null ? "loaded" : "Not loaded");
 
-
+// split the document
 const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 4000,
   chunkOverlap: 200,
 });
+const splitDocs = await splitter.splitDocuments(docs);
 
-const output = await splitter.splitDocuments(docs);
+// Load the docs into the vector store
+const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY
+  }),{});
+
+// Search for the most similar document
+// const result = await vectorStore.similaritySearch("rain", 10);
+
+const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+const res = await chain.call({
+  query: "What is the Micello WorkingSet data framework?",
+});
+console.log(res.text);
 
 
-console.log(output !== null ? "split" : "Not split");
+
+
+
